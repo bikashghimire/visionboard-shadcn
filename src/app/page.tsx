956b44628e005baec93f3f55fff8f6451f8b2eee
Bikash Ghimire <bikash.ghimire@datadrivers.fi>
2025-06-01 00:39:38 +0300

    bar chart apply

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,14 @@
-
+import AppBarChart from "@/components/AppBarChart";
 
 const HomePage = () => {
   return (
-    <div className="">
-      
+    <div className="grid grid cols-1 lg:grid-cols-2 2xl:grid-cols-4 gap-4">
+      <div className="bg-primary-foreground p-4 rounded-lg lg:col-span-2 xl:col-1 2xl:col-span-2" ><AppBarChart /></div>
+      <div className="bg-primary-foreground p-4 rounded-lg">Text</div>
+      <div className="bg-primary-foreground p-4 rounded-lg">Text</div>
+      <div className="bg-primary-foreground p-4 rounded-lg">Text</div>
+      <div className="bg-primary-foreground p-4 rounded-lg lg:col-span-2 xl:col-1 2xl:col-span-2">Text</div>
+      <div className="bg-primary-foreground p-4 rounded-lg">Text</div>
     </div>
   );
 }
