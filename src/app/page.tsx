3946ac2266a137655f67d76018d448d8547d70f7
Bikash Ghimire <bikash.ghimire@datadrivers.fi>
2025-06-02 13:59:54 +0300

    page restructure

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,10 +6,9 @@ import TodoList from "@/components/TodoList";
 
 const HomePage = () => {
   return (
-    <div className="grid grid cols-1 lg:grid-cols-2 2xl:grid-cols-4 gap-4">
+    <div className="grid cols-1 lg:grid-cols-2 2xl:grid-cols-4 gap-4">
       <div className="bg-primary-foreground p-4 rounded-lg lg:col-span-2 xl:col-1 2xl:col-span-2" ><AppBarChart /></div>
-            <div className="bg-primary-foreground p-4 rounded-lg"><CardList title="Latest Transactions"/></div>
-
+      <div className="bg-primary-foreground p-4 rounded-lg"><CardList title="Latest Transactions"/></div>
       <div className="bg-primary-foreground p-4 rounded-lg"><AppPieChart /></div>
       <div className="bg-primary-foreground p-4 rounded-lg"><TodoList /></div>
       <div className="bg-primary-foreground p-4 rounded-lg lg:col-span-2 xl:col-1 2xl:col-span-2"><AppAreaChart /></div>
