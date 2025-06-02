7ac85ff8b460d9e54d7e6aaaece2b726045fe613
Bikash Ghimire <bikash.ghimire@datadrivers.fi>
2025-06-02 15:10:28 +0300

    profile card add

--- src/app/users/page.tsx
+++ src/app/users/page.tsx
@@ -1,5 +1,7 @@
 import CardList from "@/components/CardList";
 import { Breadcrumb, BreadcrumbItem, BreadcrumbLink, BreadcrumbList, BreadcrumbPage, BreadcrumbSeparator } from "@/components/ui/breadcrumb";
+import { HoverCard, HoverCardContent, HoverCardTrigger } from "@/components/ui/hover-card";
+import { BadgeCheck } from "lucide-react";
 import Link from "next/link";
 
 const Page = () => {
@@ -29,7 +31,20 @@ const Page = () => {
                 {/* Left Side */}
                 <div className="w-full xl:w-1/3 space-y-6">
                 {/* USER BADGES CONTAINER */}
-                <div className="bg-primary-foreground p-4 rounded-lg">Badge</div>
+                <div className="bg-primary-foreground p-4 rounded-lg">
+                    <h1 className="text-xl font-semibold">User Badges</h1>
+                    <div className="flex gap-4 mt-4">
+                        <HoverCard>
+                            <HoverCardTrigger>
+                                <BadgeCheck className="rounded-full bg-blue-500/30 border-1 border-blue-500/50 p-2" size={36} />
+                            </HoverCardTrigger>
+                            <HoverCardContent>
+                                <h1 className="font-bold mb-2">Verfied User</h1>
+                                <p className="text-sm text-muted-foreground">This user has been verified by the admin.</p>
+                            </HoverCardContent>
+                        </HoverCard>
+                    </div>
+                </div>
                  {/* INFORMATION CONTAINER */}
                 <div className="bg-primary-foreground p-4 rounded-lg">Info</div>
                  {/* CARD LIST CONTAINER */}
