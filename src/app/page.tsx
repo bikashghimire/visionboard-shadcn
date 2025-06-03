fecabbf34bcc334168286ff976cca42f5ecb132d
Bikash Ghimire <bikash.ghimire@datadrivers.fi>
2025-06-03 14:36:17 +0300

    use tanstack table

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,6 @@ import AppAreaChart from "@/components/AppAreaChart";
 import AppBarChart from "@/components/AppBarChart";
 import AppPieChart from "@/components/AppPieChart";
 import CardList from "@/components/CardList";
-import TodoList from "@/components/TodoList";
 
 const HomePage = () => {
   return (
@@ -10,7 +9,6 @@ const HomePage = () => {
       <div className="bg-primary-foreground p-4 rounded-lg lg:col-span-2 xl:col-1 2xl:col-span-2" ><AppBarChart /></div>
       <div className="bg-primary-foreground p-4 rounded-lg"><CardList title="Latest Transactions"/></div>
       <div className="bg-primary-foreground p-4 rounded-lg"><AppPieChart /></div>
-      <div className="bg-primary-foreground p-4 rounded-lg"><TodoList /></div>
       <div className="bg-primary-foreground p-4 rounded-lg lg:col-span-2 xl:col-1 2xl:col-span-2"><AppAreaChart /></div>
       <div className="bg-primary-foreground p-4 rounded-lg"><CardList title="Popular Content"/></div>
     </div>
